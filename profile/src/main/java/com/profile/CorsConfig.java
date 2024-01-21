d7252370a1141c34d36361de44b3de11580f89e8
xclcok <elhammoui.mousaab@gmail.com>
2024-01-21 14:08:29 +0100

    commit backend

--- profile/src/main/java/com/profile/CorsConfig.java
+++ profile/src/main/java/com/profile/CorsConfig.java
@@ -16,7 +16,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+public class
+CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
